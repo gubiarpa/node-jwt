ede9040fdadfe31716921ee8353036f39902cfee
gubiarpa <billy.arredondo@pucp.pe>
2021-03-13 00:12:47 -0500

    Connecting to MongoDB
    https://cloud.mongodb.com

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const { connect } = require('./routes/auth');
+
+/// Connect to Database
+const _user = 'gubiarpa';
+const _pass = 'ReV6LCjWDgAngaETz1zcVjOe8Ce3sg';
+mongoose.connect(`mongodb+srv://${ _user }:${ _pass }@cluster0.vyp0a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, () => {
+    console.log('Connected to DB');
+});
 
 /// Import Routes
 const authRoute = require('./routes/auth');
